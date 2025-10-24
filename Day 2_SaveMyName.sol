115ec14e7c8f2e64fe9f4f85109b068d8ca222b5
Deerjane <vincy200610031@gmail.com>
2025-10-24 15:44:28 -0400

    Update Day 2_SaveMyName.sol

--- Day 2_SaveMyName.sol
+++ Day 2_SaveMyName.sol
@@ -2,15 +2,15 @@
 pragma solidity ^0.8.0;
 
 contract SaveMyName{
-    string name; # declare a variable with what data type it is 
+    string name; //declare a variable with what data type it is 
     string bio;
 
-    function add(string memory _name, string memory _bio) public{ # Memory means Temporary storage that only exists while the function is running.
+    function add(string memory _name, string memory _bio) public{ //Memory means Temporary storage that only exists while the function is running.
         name= _name;
         bio= _bio;
     }
 
-    function retrieve() public view returns(string memory,string memory) {  # Return is a reserved keyword
+    function retrieve() public view returns(string memory,string memory) {  //Return is a reserved keyword
         return(name,bio);
     }
 }
